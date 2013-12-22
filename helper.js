8c10c79c274f4f66c45ac0b540616ceba583c90d
Abhishek Kapatkar <cabhishek@gmail.com>
2013-12-21 19:24:03 -0500

    move all html race data files to a top level folder

--- helper.js
+++ helper.js
@@ -1,19 +1,22 @@
 var _ = require('underscore'),
     _s = require('underscore.string');
 
-function getFolderName(race){
-    var foldername  = _s.underscored(_s.sprintf('%s/%s', race.name, race.year));
+function getFolderName(race) {
+
+    var dataFolder = "htmldata";
+
+    var foldername = _s.underscored(_s.sprintf('%s/%s/%s', dataFolder, race.name, race.year));
 
     return foldername;
 }
 
-function getFileName(race){
+function getFileName(race) {
     var fileName = _s.underscored(_s.sprintf('%s_%s_page_%s.html', race.name, race.year, race.page));
 
     return fileName;
 }
 
-function createPages(race){
+function createPages(race) {
     return _.range(1, race.pages + 1);
 }
 
