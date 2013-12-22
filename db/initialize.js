6f0249841d6bd2472633f87b9d31a204c95f84b0
Abhishek Kapatkar <cabhishek@gmail.com>
2013-12-22 18:29:31 -0500

    rename to db initialize file

---
+++ db/initialize.js
@@ -0,0 +1,16 @@
+var Bookshelf = require('bookshelf');
+
+function init(){
+    Bookshelf.Mysql = Bookshelf.initialize({
+        client: 'mysql',
+
+        connection: {
+            host: '127.0.0.1',
+            user: 'root',
+            database: 'datathletics',
+            charset: 'utf8'
+        }
+    });
+}
+
+module.exports = init;
