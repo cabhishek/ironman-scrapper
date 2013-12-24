3b620ef0226aea9f153177fa88d2bed9f1f17af1
Abhishek Kapatkar <cabhishek@gmail.com>
2013-12-24 08:53:52 -0500

    stop using console.logs

--- url.js
+++ url.js
@@ -1,16 +1,17 @@
-var _s = require('underscore.string'),
-    url = require("url");
+var util = require('util'),
+    url = require("url"),
+    Log = require('log'),
+    log = new Log('info');
 
 
 module.exports = function get_url(race) {
 
     var base_url = "http://storage.athlinks.com/time.aspx";
-    var params = _s.sprintf("?eventid=%s&courseid=%s&genderpage=A%s", race.eventid, race.courseid, race.page);
+    var params = util.format("?eventid=%s&courseid=%s&genderpage=A%s", race.eventid, race.courseid, race.page);
 
     var athlinks_url = base_url + params;
 
-
-    console.log('Getting data from ->' + athlinks_url);
+    log.info('Getting data from ->%s', athlinks_url);
 
     return athlinks_url;
 };
