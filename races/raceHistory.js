90e50187efc564997fc3ef95b5d0d653cf6597a9
Abhishek Kapatkar <cabhishek@gmail.com>
2013-12-30 22:09:22 -0500

    add more races to scrape and persist

--- races/raceHistory.js
+++ races/raceHistory.js
@@ -6,12 +6,18 @@ var florida = require('./florida').florida,
     southAfrica = require('./southAfrica').southAfrica,
     australia = require('./australia').australia,
     croix = require('./croix').croix,
+    wales = require('./wales').wales,
+    laketahoe = require('./laketahoe').laketahoe,
+    asiapacificchampionships = require('./asiapacificchampionships').asiapacificchampionships,
+    kona = require('./kona').kona,
+    malaysia = require('./malaysia').malaysia,
     _ = require('underscore');
 
 module.exports = function race(raceName){
     var raceData = {};
 
     //Add data of a particular race
+    _.extend(raceData, asiapacificchampionships);
     _.extend(raceData, florida);
     _.extend(raceData, arizona);
     _.extend(raceData, cozumel);
@@ -20,6 +26,9 @@ module.exports = function race(raceName){
     _.extend(raceData, southAfrica);
     _.extend(raceData, australia);
     _.extend(raceData, croix);
+    _.extend(raceData, laketahoe);
+    _.extend(raceData, kona);
+    _.extend(raceData, malaysia);
 
     return raceData[raceName];
 };
