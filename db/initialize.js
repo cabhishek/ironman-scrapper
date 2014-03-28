4bee15ef14455f72a106ea9f3b6b8930609b96d5
Abhishek Kapatkar <cabhishek@gmail.com>
2014-03-28 11:30:18 -0400

    add live DB credentials

--- db/initialize.js
+++ db/initialize.js
@@ -4,6 +4,14 @@ function init(){
     Bookshelf.Mysql = Bookshelf.initialize({
         client: 'mysql',
 
+        // connection: {
+        //     host: 'datathletics-db-live.cc3tkob2sz1n.us-west-2.rds.amazonaws.com',
+        //     user: 'datathletics',
+        //     database: 'datathleticsdb',
+        //     password: 'F00lF00l!',
+        //     charset: 'utf8'
+        // }
+
         connection: {
             host: '127.0.0.1',
             user: 'root',
