e813fac09f9db525ca22f770c70019ffb7927150
Abhishek Kapatkar <cabhishek@gmail.com>
2014-12-30 20:57:59 -0500

    Delete old DB credentials

--- db/initialize.js
+++ db/initialize.js
@@ -3,21 +3,12 @@ var Bookshelf = require('bookshelf')
 function init(){
     Bookshelf.Mysql = Bookshelf.initialize({
         client: 'mysql',
-
         connection: {
-            host: 'datathletics-db-live.cc3tkob2sz1n.us-west-2.rds.amazonaws.com',
-            user: 'datathletics',
-            database: 'datathleticsdb',
-            password: 'F00lF00l!',
+            host: '127.0.0.1',
+            user: 'root',
+            database: 'datathletics',
             charset: 'utf8'
         }
-
-        // connection: {
-        //     host: '127.0.0.1',
-        //     user: 'root',
-        //     database: 'datathletics',
-        //     charset: 'utf8'
-        // }
     })
 }
 
